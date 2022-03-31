df39fabb135bf05daaf1b4c5951319bda5060d94
Ludo <ludo.meeuws@gmail.com>
2022-03-31 11:24:10 +0200

    added mqtt client and its basic functionality

--- modular_steering/mqttclient.py
+++ modular_steering/mqttclient.py
@@ -5,3 +5,35 @@ import paho.mqtt.client as mqtt
 #constants
 MQTT_ADDRESS = '192.168.1.104'
 MQTT_TOPIC = 'test/message'
+
+
+class MqttClient():
+    
+
+    def __init__(self, address_broker, on_message):
+        self.address_broker = address_broker
+        self.on_message = on_message
+
+        print("new broker with address broker: " + self.address_broker)
+    
+        self.client = mqtt.Client("ABS")
+        
+    def start(self, topics):
+        self.client.connect(MQTT_ADDRESS)
+        
+        for t in topics:
+            print(t)
+            self.client.subscribe(t)
+            #self.client.message_callback_add()
+
+        self.client.on_message = self.callback
+        
+        self.client.loop_start()
+
+
+
+    def callback(self, client, userdata, message):
+        print("Received message '" + str(message.payload) + "' on topic '"
+        + message.topic + "' with QoS " + str(message.qos))
+
+
