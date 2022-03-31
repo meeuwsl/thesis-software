78b8118851dff508b9d695dcb8512db363aba1c4
Ludo <ludo.meeuws@gmail.com>
2022-03-31 11:24:44 +0200

    further improvement

--- modular_steering/modules/cameramodule.py
+++ modular_steering/modules/cameramodule.py
@@ -1,14 +1,19 @@
 from modular_steering.interface import ModuleInterface
 
 class CameraModule:
+    def __init__(self):
+        super().__init__()
+        print("cameramodule initialised")
+
+
     """Extract text from a PDF."""
-    def onmsgrecv(self, path: str, file_name: str) -> str:
-        """Overrides FormalParserInterface.load_data_source()"""
+    def onmsgrecv((self, message, payload)):
         pass
 
-    def onstop(self, full_file_path: str) -> dict:
-        """Overrides FormalParserInterface.extract_text()"""
+    def onstop(self):
+        print("stop cameramodule")
         pass
 
     def onstart(self):
         print("start cameramodule")
+        pass
